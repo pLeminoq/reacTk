1ea24cbf6abcd5d52f233ea543c4562a0232dfb7
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2025-04-02 09:23:55 +0200

    add implementation that fixes bug and rename to async_once (old one is marked deprecated)

--- reacTk/decorator/__init__.py
+++ reacTk/decorator/__init__.py
@@ -1,9 +1,10 @@
-from .asynchron import asynchron
+from .asynchron import asynchron, async_once
 from .recurrency_filter import recurrency_filter
 from .stateful import stateful
 
 __all__ = [
     "asynchron",
+    "async_once",
     "recurrency_filter",
     "stateful",
 ]
--- reacTk/decorator/asynchron.py
+++ reacTk/decorator/asynchron.py
@@ -2,9 +2,15 @@ from dataclasses import dataclass
 import functools
 import threading
 from typing import Callable, Generic, Optional, ParamSpec
+from warnings import warn
 
 
 P = ParamSpec("P")
+
+
+def is_instance_method(func: Callable) -> bool:
+    params = list(inspect.signature(func).parameters.values())
+    return len(params) > 0 and params[0].name == "self"
 
 
 @dataclass
@@ -19,6 +25,7 @@ class AsyncData(Generic[P]):
 
 
 def asynchron(func: Callable[P, None]) -> Callable[P, None]:
+    warn("`asynchron` decorator is deprecated. Use `async_once` instead")
     async_data = AsyncData()
 
     def trigger_pending() -> None:
@@ -50,5 +57,62 @@ def asynchron(func: Callable[P, None]) -> Callable[P, None]:
             ):
                 async_data.pending_call = threading.Thread(target=trigger_pending)
                 async_data.pending_call.start()
+
+    return wrapper
+
+def async_once(func: Callable[P, None]) -> Callable[P, None]:
+    # async_data = AsyncData()
+    async_data_map_lock = threading.Lock()
+    async_data_map = {}
+
+    # def wait():
+    #     print(f"Wait for func ...{func.__name__=}")
+    #     for i, async_data in enumerate(async_data_map.values()):
+    #         print(f" - Lock {i=}")
+    #         with async_data.lock:
+    #             pass
+    #
+    # func.__wait = wait
+
+    func.__async_data_map = async_data_map
+
+    def trigger_pending(async_data) -> None:
+        async_data.current_call.join()
+
+        with async_data.lock:
+            async_data.pending_call = None
+
+            async_data.current_call = threading.Thread(
+                target=func, args=async_data.last_args, kwargs=async_data.last_kwargs
+            )
+            async_data.current_call.start()
+
+    @functools.wraps(func)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
+        print(f"Call to func {func.__name__=}")
+        with async_data_map_lock:
+            _self = args[0]
+            if _self not in async_data_map:
+                async_data_map[_self] = AsyncData()
+            async_data = async_data_map[_self]
+
+        with async_data.lock:
+            if async_data.current_call is None:
+                async_data.current_call = threading.Thread(
+                    target=func, args=args, kwargs=kwargs
+                )
+                print(f"Start current call")
+                async_data.current_call.start()
+                return
+
+            async_data.last_args = args
+            async_data.last_kwargs = kwargs
+            if (
+                async_data.pending_call is None
+                or async_data.pending_call.is_alive() is False
+            ):
+                async_data.pending_call = threading.Thread(target=trigger_pending, args=[async_data])
+                async_data.pending_call.start()
+
 
     return wrapper
